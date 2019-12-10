3bd8830ae86c902fc1762de67ca5bcd38f52956e
whoover <whoover@nemours.org>
2019-12-10 14:21:21 -0500

    Added runner

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # labrat
 Run [@hapi/lab](https://github.com/hapijs/lab) tests on vanilla test suites
+
+`npm install -D @ugate/labrat`
+
+__test/lib/tester.js__:
+```js
+// vanilla test suite (all tests should be static)
+
+const { Labrat, LOGGER } = require('@ugate/labrat');
+
+class Tester {
+
+  static myTest1() {
+    if (LOGGER.info) LOGGER.info('Show this when info level enabled');
+    // test here
+  }
+
+  async static myTest2() {
+    // test here
+  }
+}
+
+// when not ran in a test runner execute static Tester functions (excluding what's passed into Labrat.run) 
+if (!Labrat.usingTestRunner()) {
+  (async () => await Labrat.run(Tester))();
+}
+```
+
+__test/tester.js__:
+```js
+const Lab = require('@hapi/lab');
+const Tester = require('./lib/tester');
+const lab = Lab.script();
+exports.lab = lab;
+
+const plan = `Demo`;
+
+lab.experiment(plan, () => {
+  lab.test(`${plan}: Test #1`, { timeout: 1000 }, Tester.myTest1);
+  lab.test(`${plan}: Test #2`, { timeout: 1000 }, Tester.myTest2);
+});
+```
+
+#### Log Levels
+- `-NODE_ENV=dev` - All levels/functions included in _console_
+- `-NODE_ENV=test` - Includes _console.info_, _console.warn_, _console.error_
+- Omit or set to another environment to disable logging
+
+#### Running Tests
+Tests can be ran in a `Node.js` command or in [@hapi/lab](https://github.com/hapijs/lab).
+
+Run in node:
+`node test/lib/tester.js -NODE_ENV=test`
+
+Run _myTest1_ in node:
+`node test/lib/tester.js -NODE_ENV=test myTest1`
+
+Run in [@hapi/lab](https://github.com/hapijs/lab):
+`"node_modules/.bin/lab" test/tester.js -v`
+
+Run _myTest1_ in [@hapi/lab](https://github.com/hapijs/lab):
+`"node_modules/.bin/lab" test/tester.js -vi 1`
+
+Run _myTest2_ in [@hapi/lab](https://github.com/hapijs/lab):
+`"node_modules/.bin/lab" test/tester.js -vi 2`
---
+++ index.js
@@ -0,0 +1,122 @@
+'use strict';
+
+const argv = process.argv.slice(2);
+const log = process.env.NODE_ENV === 'test' || argv.includes('-NODE_ENV=test') ?
+  { info: console.info, warn: console.warn, error: console.error } : 
+  process.env.NODE_ENV === 'dev' || argv.includes('-NODE_ENV=dev') ? console : {};
+// TODO : ESM comment the following line...
+exports.LOGGER = log;
+// TODO : ESM uncomment the following lines...
+// export const LOGGER = log;
+
+
+// TODO : ESM uncomment the following line...
+// export
+class Labrat {
+
+  /**
+   * Async test that will either `resolve`/`reject` after a given amount of time
+   * @async
+   * @param {Integer} delay The delay in milliseconds to wait before resolving/rejecting
+   * @param {*} [val] The value to return when resolved or error message/Error when rejecting
+   * @param {Boolean} [rejectIt] `true` to reject, otherwise resolve
+   */
+  static wait(delay, val, rejectIt) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (rejectIt) reject(val instanceof Error ? val : new Error(val));
+        else resolve(val);
+      }, delay);
+    });
+  }
+
+  /**
+   * Runs a function from a class using `return Clazz[process.argv[2]](...args)`
+   * __or__ runs through all of the `async` functions from `Object.getOwnPropertyNames(Clazz)` via
+   * `await Clazz[propertyName](...args)` other than function names that are found in `excludes` or
+   * `before`, `beforeEach`, `after`, `afterEach`. If the _static_ method ends with "__Error__", execution
+   * will assume that the function will _throw_.
+   * @ignore
+   * @param {Object} Clazz The class the test running will be running for
+   * @param {String[]} [excludes] Function names to exclude from execution (only used when executing all)
+   * @param {*[]} [args] Any additional arguments to pass to function(s) that are executed
+   * @returns {Object} The return value(s) from the designated function call with each property
+   * name set to the name of the function executed and the value as the return value from the call
+   */
+  static async run(Clazz, excludes, ...args) {
+    let prps = [], frx = /^[a-z]/i;
+    for (let arg of argv) {
+      if (frx.test(arg)) prps.push(arg);
+    }
+    if (!prps.length) prps = Object.getOwnPropertyNames(Clazz);
+    if (log && log.info) log.info(`Preparing execution of ${Clazz.name}.${prps.join(`, ${Clazz.name}.`)}`);
+
+    const excls = ['before', 'beforeEach', 'after', 'afterEach'], execr = {}, rtn = {};
+    const prefix1 = '\n\x1b[37m\x1b[44m============>> ', prefix2 = ' <<============\x1b[0m\x1b[40m';
+    for (let enm of excls) {
+      execr[enm] = typeof Clazz[enm] === 'function' ? Clazz[enm] : null;
+    }
+
+    if (execr['before']) {
+      if (log.info) log.info(`${prefix1}Executing: ${Clazz.name}.before(${args.join(',')})${prefix2}`);
+      rtn['before'] = await execr['before'](...args);
+    }
+    let error;
+    for (let prop of prps) {
+      error = null;
+      if (typeof Clazz[prop] !== 'function' || (excludes && excludes.includes(prop)) || excls.includes(prop)) continue;
+      if (execr['beforeEach']) {
+        if (log.info) log.info(`${prefix1}Executing: ${Clazz.name}.beforeEach(${args.join(',')})${prefix2}`);
+        rtn['beforeEach'] = await execr['beforeEach'](...args);
+      }
+      try {
+        if (log.info) log.info(`${prefix1}Executing: await ${Clazz.name}.${prop}(${args.join(',')})${prefix2}`);
+        rtn[prop] = await Clazz[prop](...args);
+        if (log.info) log.info(`\nExecution complete for: await ${Clazz.name}.${prop}(${args.join(',')})`);
+      } catch (err) {
+        if (prop.endsWith('Error')) {
+          rtn[prop] = err;
+          if (log.info) log.info(`\nExecution complete for: await ${Clazz.name}.${prop}(${args.join(',')}) -> Expected error: "${err.message}"`);
+        } else {
+          if (log.error) log.error(err);
+          error = err;
+          error.cause = `Execution failed for: await ${Clazz.name}.${prop}(${args.join(',')})`;
+        }
+      }
+      if (execr['afterEach']) {
+        if (log.info) log.info(`${prefix1}Executing: ${Clazz.name}.afterEach(${args.join(',')})${prefix2}`);
+        try {
+          rtn['afterEach'] = await execr['afterEach'](...args);
+        } catch (err) {
+          if (error && log.error) log.error(err);
+          if (!error) throw err;
+        }
+      }
+      if (error) break;
+    }
+    if (execr['after']) {
+      if (log.info) log.info(`${prefix1}Executing: ${Clazz.name}.after(${args.join(',')})${prefix2}`);
+      try {
+        rtn['after'] = await execr['after'](...args);
+      } catch (err) {
+        if (error && log.error) log.error(err);
+        if (!error) throw err;
+      }
+    }
+    if (error) {
+      if (error.cause) throw new Error(`\n${error.cause} ... Execution aborted!`);
+      throw error;
+    }
+    return rtn;
+  }
+
+  /**
+   * @returns {Boolean} `true` when the process is being ran from a _test utility_
+   */
+  static usingTestRunner() {
+    return process.mainModule.filename.endsWith('lab');
+  }
+}
+
+// TODO : ESM remove the following line...
+exports.Labrat = Labrat;
